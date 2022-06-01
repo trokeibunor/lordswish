f9ffc4b8fd16e35e73b301a2be687295de409de6
Emmanuel Okeibunor <okeibunoremma@gmail.com>
2022-06-01 12:17:41 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav>
+    <img
+      src="./assets/images/illustration_top.svg"
+      alt=""
+      class="illustration"
+      srcset=""
+    />
+    <h4 class="name">LORD'SWISH.</h4>
+    <nav-link>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <a href="#">Portfolio</a>
+      <a href="#">Contact me</a>
+    </nav-link>
+  </nav>
+  <RouterView />
+</template>
+
+<script>
+import { RouterLink, RouterView } from "vue-router";
+
+export default {
+  components: {
+    RouterLink,
+    RouterView,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/base.css";
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+body {
+  background-attachment: fixed;
+  font-family: "Poppins", sans-serif;
+  background-color: hsla(239, 62%, 15%, 1);
+  color: #fff;
+}
+#app {
+  min-width: 100vw;
+  min-height: 100vh;
+}
+#app::before {
+  box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.2);
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+nav {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 16px;
+  margin: 0px 8px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <section id="hero"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home-view ",
+};
+</script>
+
+<style lang="scss" scoped></style>
