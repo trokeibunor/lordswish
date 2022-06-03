2bd3cb7c42d4518733f4474f49343042b0e1e2a0
Emmanuel Okeibunor <okeibunoremma@gmail.com>
2022-06-04 00:41:18 +0100

    changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,106 @@
     <section id="featuredworks">
       <h3 id="featured">Featured Works</h3>
       <hr />
+      <h4 id="case">Case Study</h4>
+      <!-- card section -->
+      <div class="card-grid">
+        <div class="cs-card">
+          <img
+            src="https://picsum.photos/200/300"
+            class="card-img"
+            alt=""
+            srcset=""
+          />
+          <div class="card-desc">
+            <h2>Kofamed</h2>
+            <a href="#">UI/UX Design, UX Research, Motion Design</a>
+            <p class="note">
+              I am a creative UI/UX designer, I help individuals and companies
+              to design intuitive and usable digital products.
+            </p>
+          </div>
+        </div>
+        <div class="cs-card">
+          <img
+            src="https://picsum.photos/200/300"
+            class="card-img"
+            alt=""
+            srcset=""
+          />
+          <div class="card-desc">
+            <h2>Kofamed</h2>
+            <a href="#">UI/UX Design, UX Research, Motion Design</a>
+            <p class="note">
+              I am a creative UI/UX designer, I help individuals and companies
+              to design intuitive and usable digital products.
+            </p>
+          </div>
+        </div>
+        <div class="cs-card">
+          <img
+            src="https://picsum.photos/200/300"
+            class="card-img"
+            alt=""
+            srcset=""
+          />
+          <div class="card-desc">
+            <h2>Kofamed</h2>
+            <a href="#">UI/UX Design, UX Research, Motion Design</a>
+            <p class="note">
+              I am a creative UI/UX designer, I help individuals and companies
+              to design intuitive and usable digital products.
+            </p>
+          </div>
+        </div>
+        <div class="cs-card">
+          <img
+            src="https://picsum.photos/200/300"
+            class="card-img"
+            alt=""
+            srcset=""
+          />
+          <div class="card-desc">
+            <h2>Kofamed</h2>
+            <a href="#">UI/UX Design, UX Research, Motion Design</a>
+            <p class="note">
+              I am a creative UI/UX designer, I help individuals and companies
+              to design intuitive and usable digital products.
+            </p>
+          </div>
+        </div>
+        <div class="cs-card">
+          <img
+            src="https://picsum.photos/200/300"
+            class="card-img"
+            alt=""
+            srcset=""
+          />
+          <div class="card-desc">
+            <h2>Kofamed</h2>
+            <a href="#">UI/UX Design, UX Research, Motion Design</a>
+            <p class="note">
+              I am a creative UI/UX designer, I help individuals and companies
+              to design intuitive and usable digital products.
+            </p>
+          </div>
+        </div>
+        <div class="cs-card">
+          <img
+            src="https://picsum.photos/200/300"
+            class="card-img"
+            alt=""
+            srcset=""
+          />
+          <div class="card-desc">
+            <h2>Kofamed</h2>
+            <a href="#">UI/UX Design, UX Research, Motion Design</a>
+            <p class="note">
+              I am a creative UI/UX designer, I help individuals and companies
+              to design intuitive and usable digital products.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -101,11 +201,73 @@ export default {
   width: 60%;
   margin: 4rem auto 2rem auto;
   #featured {
+    font-size: 30px;
+    line-height: 130%;
+    /* identical to box height, or 39px */
+
+    text-decoration-line: underline;
     text-align: center;
     text-decoration: underline;
     margin-bottom: 20px;
     hr {
       color: #fff;
+    }
+  }
+  #case {
+    margin: 32px 0px;
+    text-decoration: underline;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 130%;
+  }
+}
+.card-grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  grid-gap: 48px;
+  margin: 16px 0px;
+  .cs-card {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    .card-img {
+      width: 100%;
+      object-fit: cover;
+      height: 305px;
+      border-radius: 20px 20px 0px 0px;
+    }
+    .card-desc {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-start;
+      padding: 0px 0px 0px 16px;
+      gap: 16px;
+      background: #0f2b5f;
+      border-radius: 0px 0px 20px 20px;
+      h2 {
+        margin: 8px 0px;
+        font-weight: 700;
+        font-size: 30px;
+        line-height: 130%;
+        color: #ffffff;
+      }
+      a {
+        margin: 2px 0px;
+        font-weight: 500;
+        font-size: 17px;
+        line-height: 143.2%;
+        color: #c961de;
+      }
+      p {
+        margin: 8px 0px;
+        font-weight: 500;
+        font-size: 17px;
+        line-height: 143.2%;
+        color: #c4c4c4;
+      }
     }
   }
 }
